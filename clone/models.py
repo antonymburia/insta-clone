198404ffc0aee89ad1ca98fa6e2938d7de4069d9
antonymburia <antonymburia10@gmail.com>
2022-04-03 02:10:08 +0300

    create a post model class

--- clone/models.py
+++ clone/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post_image = models.ImageField(upload_to='articles/', blank=True)
--- insta/urls.py
+++ insta/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.contrib.auth import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('clone.urls')),
     path('accounts/', include('registration.backends.simple.urls')),
+    path('logout/', views.logout, {"next_page": '/'}),
 
 ]
