e1a98403bb338cbcf5357fcc399186f97fd6b81b
antonymburia <antonymburia10@gmail.com>
2022-04-03 03:30:07 +0300

    login feature works

--- clone/views.py
+++ clone/views.py
@@ -3,5 +3,6 @@ from django.http  import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def home(request):
     return render(request,'index.html')
